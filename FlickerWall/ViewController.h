b8b681203c6d1e82adf5d8a6d2ce5917fc080be2
Mario Barbosa <mario.bajr@gmail.com>
2013-02-05 18:22:45 -0300

    Initial Commit

---
+++ FlickerWall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlickerWall
+//
+//  Created by Mario Barbosa on 2/5/13.
+//  Copyright (c) 2013 Mario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FlickerWall/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FlickerWall
+//
+//  Created by Mario Barbosa on 2/5/13.
+//  Copyright (c) 2013 Mario. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
